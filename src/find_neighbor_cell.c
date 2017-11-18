7c67db3381ff68cc0f262317256af94097d7a137
yexin <x1314aq@sina.cn>
2017-11-18 11:08:46 +0800

    修复了surf_init函数使用new运算符进行动态内存分配的问题；find_neighbor_cell还是空实现

---
+++ src/find_neighbor_cell.c
@@ -0,0 +1,9 @@
+//
+// Created by xaq on 11/18/17.
+//
+
+#include "geometry.h"
+
+void find_neighbor_cell(){
+
+}
--- src/surface.c
+++ src/surface.c
@@ -5,7 +5,7 @@
 #include "surface.h"
 
 surface_t *surf_init(){
-    surface_t *_new_surf = new surface_t;
+    surface_t *_new_surf = (surface_t *) malloc(sizeof(surface_t));
     memset(_new_surf, 0x0, sizeof(surface_t));
     return _new_surf;
 }
