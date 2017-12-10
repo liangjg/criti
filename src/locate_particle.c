0443c3dd9c7ac04b0b635efc3968a7cdb583d3cf
yexin <x1314aq@sina.cn>
2017-12-10 16:28:23 +0800

    locate_particle进行了格式调整

--- src/locate_particle.c
+++ src/locate_particle.c
@@ -13,8 +13,10 @@ extern map *base_cells;
 extern map *base_surfs;
 
 int locate_particle(particle_state_t *par_state, const double pos[3], const double dir[3]){
-    int univ_index = 0;
-    int found_cell = -1;
+    universe_t *univ;
+    cell_t *cell;
+    int level, univ_index, cell_index, found_cell;
+    int filled_univ, lat_univ;
     double local_pos_temp[3];
     double local_dir_temp[3];
 
@@ -23,10 +25,13 @@ int locate_particle(particle_state_t *par_state, const double pos[3], const doub
         local_dir_temp[i] = dir[i];
     }
 
-    int level = 0;
+    univ_index = 0;
+    found_cell = -1;
+    level = 0;
 
-    universe_t *univ;
-    cell_t *cell;
+    /* 清空当前univs和cells，全部重新定位 */
+    vector_clear(&par_state->loc_univs);
+    vector_clear(&par_state->loc_cells);
 
     while(1){
         if(++level > 99){
@@ -46,20 +51,18 @@ int locate_particle(particle_state_t *par_state, const double pos[3], const doub
             }
 
             vector_push_back(&par_state->loc_cells, &lat_index);
-            int lat_univ = univ->filled_lat_univs[lat_index];
+            lat_univ = univ->filled_lat_univs[lat_index];
             move_to_origin_lat(univ, lat_index, local_pos_temp);
             trans_univ_coord((universe_t *) map_get(base_univs, lat_univ), local_pos_temp, local_dir_temp);
             univ_index = lat_univ;
 
         } else{    /* current universe has some cells and has no lattice */
             for(size_t i = 0; i < vector_size(&univ->cells); i++){
-                int cell_index = *(int *) vector_at(&univ->cells, i);
-
+                cell_index = *(int *) vector_at(&univ->cells, i);
                 cell = (cell_t *) map_get(base_cells, cell_index);
 
                 if(particle_is_in_cell(cell, local_pos_temp, local_dir_temp)){
                     vector_push_back(&par_state->loc_cells, &cell_index);
-
                     if(cell->fill < 0){    /* current cell is a simple cell which has no fills */
                         for(int j = 0; j < 3; j++){
                             par_state->loc_pos[j] = local_pos_temp[j];
@@ -68,7 +71,7 @@ int locate_particle(particle_state_t *par_state, const double pos[3], const doub
                         found_cell = cell_index;
                         break;
                     } else{    /* current cell has a universe filled in */
-                        int filled_univ = cell->fill;
+                        filled_univ = cell->fill;
                         trans_univ_coord((universe_t *) map_get(base_univs, filled_univ), local_pos_temp,
                                          local_dir_temp);
                         univ_index = filled_univ;
